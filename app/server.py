6a617152a71ec9cd301434cb00e533363c55dce5
auwal amshi <25012539+amshoo@users.noreply.github.com>
2019-10-01 13:03:07 +0800

    classes

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Acne disease','Chickenpox disease','Eczema disease','Hives disease','Warts disease','Measles disease','Psoriasis disease']
 path = Path(__file__).parent
 
 app = Starlette()

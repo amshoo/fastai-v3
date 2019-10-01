0886f6c106d66b83d754d64967e90ddbb2907cda
auwal amshi <25012539+amshoo@users.noreply.github.com>
2019-10-01 14:08:21 +0800

    add link

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/open?id=1-osEVBrXckXCeebVng-RzywE8FGNaq7E'
 export_file_name = 'export.pkl'
 
 classes = ['Acne disease','Chickenpox disease','Eczema disease','Hives disease','Warts disease','Measles disease','Psoriasis disease']
